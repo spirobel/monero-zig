72b5944b9f33c85592b7e492d4b54aedb2f13326
spirobel <spirobel@protonmail.com>
2022-06-21 02:05:13 +0800

    get cwd during build

--- build.zig
+++ build.zig
@@ -3,12 +3,15 @@ const std = @import("std");
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+            const cwd = b.build_root;
+
+    std.log.info("{s}", .{cwd});
     const mode = b.standardReleaseOptions();
 
     //https://zig.news/samhattangady/porting-my-game-to-the-web-4194
     const target = std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-wasi" }) catch unreachable;
     const exe = b.addExecutable("monero-zig", "src/main.zig");
-    const lib = b.addSharedLibrary("minus", null,.unversioned);
+    const lib = b.addStaticLibrary("minus", null);
         lib.addSystemIncludeDir("/home/test/Projects/zig/monero-zig/wasi-sdk-14.0/share/wasi-sysroot");
         lib.addSystemIncludeDir("src");
      lib.addCSourceFile("src/minus.cpp", &[_][]const u8{
@@ -16,7 +19,7 @@ pub fn build(b: *std.build.Builder) void {
      "-Wextra",
      "-Werror",
  }); 
-    //lib.linkLibCpp();
+    lib.linkLibCpp();
     lib.setTarget(target);
     lib.setBuildMode(mode);
     lib.install();

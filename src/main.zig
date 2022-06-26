72bd2dddb9dba6bb63307affa9811fbe4e267a3b
spirobel <spirobel@protonmail.com>
2022-06-26 15:50:18 +0800

    print from cpp

--- src/main.zig
+++ src/main.zig
@@ -4,20 +4,14 @@ const c = @cImport({
 });
 
 pub fn main() !void {
-    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    const gpa = general_purpose_allocator.allocator();
-    const args = try std.process.argsAlloc(gpa);
-    defer std.process.argsFree(gpa, args);
-
-    for (args) |arg, i| {
-        std.debug.print("{}: {s}\n", .{ i, arg });
-    }
     const bla = c.monero_base58_encode_wrapper();
-    std.debug.print("c : {}: \n", .{ bla });
+    const stdout = std.io.getStdOut().writer();
+        try stdout.print("Hello, {s}!\n", .{"world"});
 
+std.debug.print("c : {}: \n", .{ bla });
 }
     //std.log.info("{}",.{c.minus_c()}) ;
 export fn monero_base58_encode() void {
-    const bla = c.monero_base58_encode_wrapper();
-    std.debug.print("c : {}: \n", .{ bla });
+   // const bla = c.monero_base58_encode_wrapper();
+//std.debug.print("c : {}: \n", .{ bla });
 }
--- src/moneroWrapper.cpp
+++ src/moneroWrapper.cpp
@@ -8,6 +8,8 @@ int monero_base58_encode_wrapper(void)
     std::string s1( "What is the sound of one clam napping?");
     std::string b = tools::base58::encode(s1);
     //return tools::base58::encode(s1).size();
+    printf("blablabla");
+
     return s1.size();
 }
 }
